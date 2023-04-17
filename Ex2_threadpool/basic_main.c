2ad95e17637b3e22acc04f472260c99a137a5988
Your Name <you@example.com>
2023-04-18 01:14:59 +0300

    started ex2

--- Ex2_threadpool/basic_main.c
+++ Ex2_threadpool/basic_main.c
@@ -6,11 +6,28 @@ int main(int argc, char *argv[])
 	char data[] = "my text to encrypt";
 	int key = 12;
 
-	encrypt(data,key);
-	printf("encripted data: %s\n",data);
+	encrypt(data, key);
+	printf("encripted data: %s\n", data);
 
-	decrypt(data,key);
-	printf("decripted data: %s\n",data);
+	// decrypt(data,key);
+	// printf("decripted data: %s\n",data);
+	printf("\n\n\n");
+	char data1[] = "my ";
+	char data2[] = "text ";
+	char data3[] = "to ";
+	char data4[] = "encrypt";
+
+	encrypt(data1, key);
+	printf("encripted data: %s\n", data1);
+	encrypt(data2, key);
+	printf("encripted data: %s\n", data2);
+	encrypt(data3, key);
+	printf("encripted data: %s\n", data3);
+	encrypt(data4, key);
+	printf("encripted data: %s\n", data4);
+
+	// decrypt(data,key);
+	// printf("decripted data: %s\n",data);
 
 	return 0;
 }
--- Ex2_threadpool/stdin_main.c
+++ Ex2_threadpool/stdin_main.c
@@ -2,45 +2,84 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+#define MAX_WORKING_THREADS 3
+#define MAX_POOL_SIZE 100
+
+typedef struct
+{
+	char data[512];
+	int key;
+	int id;
+} encrypt_data;
+
+void *encrypt_thread(void *arg)
+{
+	encrypt_data *data = (encrypt_data *)arg;
+	printf("thread %d -> data: %s\n\n", data->id, data->data);
+	encrypt(data->data, data->key);
+	printf("thread %d -> encripted data: %s\n\n", data->id, data->data);
+	return NULL;
+}
 
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-	    printf("usage: key < file \n");
-	    printf("!! data more than 1024 char will be ignored !!\n");
-	    return 0;
+		printf("usage: key < file \n");
+		printf("!! data more than 1024 char will be ignored !!\n");
+		return 0;
 	}
 
 	int key = atoi(argv[1]);
-	printf("key is %i \n",key);
+	printf("key is %i \n", key);
 
 	char c;
 	int counter = 0;
-	int dest_size = 1024;
-	char data[dest_size]; 
-	
+	pthread_t pool[MAX_POOL_SIZE];
+	encrypt_data data1;
+	encrypt_data data2;
+	data1.key = key;
+	data2.key = key;
+	pthread_t thread1, thread2;
+	int i = 1;
 
 	while ((c = getchar()) != EOF)
 	{
-	  data[counter] = c;
-	  counter++;
+		if (counter % 1024 == 0)
+		{
+		}
 
-	  if (counter == 1024){
-		encrypt(data,key);
-		printf("encripted data: %s\n",data);
-		counter = 0;
-	  }
+		if (counter < 512)
+		{
+			data1.data[counter] = c;
+		}
+		else
+		{
+			data2.data[counter - 512] = c;
+		}
+
+		counter++;
+
+		if (counter == 512)
+		{
+			data1.id = 1;
+			// data1.data[counter] = '\0';
+			printf("thread1 starting!\n");
+			pthread_create(&thread1, NULL, encrypt_thread, (void *)&data1);
+		}
+
+		if (counter == 1023)
+		{
+			data2.id = 2;
+			printf("thread2 starting!\n");
+			pthread_create(&thread2, NULL, encrypt_thread, (void *)&data2);
+		}
+		// printf("counter is: %d\n", counter);
 	}
-	
-	if (counter > 0)
-	{
-		char lastData[counter];
-		lastData[0] = '\0';
-		strncat(lastData, data, counter);
-		encrypt(lastData,key);
-		printf("encripted data:\n %s\n",lastData);
-	}
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
 
 	return 0;
 }
